c2b14fa28137a59bc7ad91bf6fcc12a092cdd7ff
ramalakshmi8 <ramalakshumma482@gmail.com>
2026-03-07 03:34:16 +0530

    Added favicon and fixed path

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar.jsx";
 import { useNavigate } from "react-router-dom";
 import { motion } from "motion/react";
-import img from "../assets/image.png";
+import img from "../assets/img1.png";
 import Footer from "../components/Footer.jsx";
 const Home = () => {
   const navigate = useNavigate();
